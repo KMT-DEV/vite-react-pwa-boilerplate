1175f32f28cf1173289597dd9200507230160280
hsabahy <hsabahy.gmail.com>
2024-04-10 20:13:43 +0200

    Init App

---
+++ README.md
@@ -0,0 +1,21 @@
+ <h3 align="center">Vite React PWA Boilerplate (TypeScript)</h3>
+
+## What's included
+
+-   Vite
+-   React
+-   PWA Plugins
+-   MUI
+-   MUI Icons
+-   Robot Font (English Language)
+-   Tajawal (Arabic Language)
+-   Axios
+-   i18next (for translations)
+-   React Router Dom
+
+## Installation
+
+-   Clone project
+-   Run `npm install`
+
+## Thanks
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/tajawal/200.css';
+import '@fontsource/tajawal/300.css';
+import '@fontsource/tajawal/400.css';
+import '@fontsource/tajawal/500.css';
+import '@fontsource/tajawal/700.css';
+import '@fontsource/tajawal/800.css';
+import '@fontsource/tajawal/900.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
